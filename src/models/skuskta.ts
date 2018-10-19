6bea7a96c13d669d1d203cd1b7c8af3784b53e85
Miroslav Gajdos <miroslav.gajdos@neoris.com>
2018-10-20 00:12:32 +0200

    modify scripts

--- src/models/skuskta.ts
+++
@@ -1,3 +0,0 @@
-export class Ahoj {
-    public a = 'ahoj'
-}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,5 @@
 import express from 'express'
-import { Ahoj } from '../models/skuskta';
-
-const ahoj = new Ahoj()
 
 const app = express()
 
-app.listen(8000, () => console.log('Server running on 8000!!'))
-
+app.listen(8000, () => console.log('Server running on 8000'))
