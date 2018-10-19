d5c8fac7b201ff21901a05924187c3e2db49eb56
Miroslav Gajdos <miroslav.gajdos@neoris.com>
2018-10-19 23:39:49 +0200

    setup nodemon

---
+++ src/models/skuskta.ts
@@ -0,0 +1,3 @@
+export class Ahoj {
+    public a = 'ahoj'
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import { Ahoj } from '../models/skuskta';
+
+const ahoj = new Ahoj()
 
 const app = express()
 
-app.listen(8000, () => console.log('Server running on 8000!'))
+app.listen(8000, () => console.log('Server running on 8000!!'))
 
-console.log('lo')
