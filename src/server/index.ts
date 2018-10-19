f85ea7f94effeb0430678a7357aa67febc4bbad3
Miroslav Gajdos <miroslav.gajdos@neoris.com>
2018-10-19 23:04:19 +0200

    remove old tslint rules

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.listen(8000, () => console.log('Server running on 8000!'));
+app.listen(8000, () => console.log('Server running on 8000!'))
 
 console.log('lo')
